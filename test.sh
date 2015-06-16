4824a98f29743d37530353c81b41794d99cd470d
Yuya Unno <unnonouno@gmail.com>
2015-06-17 00:03:31 +0900

    Call coveralls after test

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@ cd chainer
 python setup.py install
 pip install `chainer-cuda-requirements`
 
-pip install nose coverage
+pip install nose coverage coveralls
 nosetests --with-coverage --cover-branches --cover-package=chainer
 coverage xml -i
+
+if [ $COVERALLS_REPO_TOKEN ]; then
+  apt-get -y install git
+  coveralls
+fi
