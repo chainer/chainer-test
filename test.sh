b22bb62869188dda08081a9baab6bfe7b2a7c11f
Yuya Unno <unnonouno@gmail.com>
2015-09-12 19:13:56 +0900

    Fix cover-package of coverage

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ python setup.py -q install
 pip install -q nose mock coverage coveralls
 
 nosetests --processes=12 --process-timeout=10000 --with-coverage --cover-branches --cover-package=cupy tests/cupy_tests
-nosetests --with-coverage --cover-branches --cover-package=chainer tests/chainer_tests
+nosetests --with-coverage --cover-branches --cover-package=chainer,cupy tests/chainer_tests
 
 coverage xml -i
 
