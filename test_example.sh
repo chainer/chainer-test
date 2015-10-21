67ca6003ccb35a14939199827b79903cbf1a27d7
Yuya Unno <unnonouno@gmail.com>
2015-10-21 10:46:58 +0900

    Show coverage of examples

--- test_example.sh
+++ test_example.sh
@@ -8,93 +8,81 @@ if [ -e cuda_deps/setup.py ]; then
 fi
 
 apt-get install -y libjpeg-dev zlib1g-dev
-pip install pillow
+pip install coverage pillow
 
-cd examples
+run="coverage run -a --branch"
 
 # mnist
 echo "Running mnist example"
-cd mnist
 
 # change epoch
-sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" train_mnist.py
-sed -i -E "s/^n_units\s*=\s*[0-9]+/n_units = 10/" train_mnist.py
+sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" examples/mnist/train_mnist.py
+sed -i -E "s/^n_units\s*=\s*[0-9]+/n_units = 10/" examples/mnist/train_mnist.py
 
-sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" train_mnist_model_parallel.py
-sed -i -E "s/^n_units\s*=\s*[0-9]+/n_units = 10/" train_mnist_model_parallel.py
+sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" examples/mnist/train_mnist_model_parallel.py
+sed -i -E "s/^n_units\s*=\s*[0-9]+/n_units = 10/" examples/mnist/train_mnist_model_parallel.py
 
-python train_mnist.py
-python train_mnist.py --gpu=0
-python train_mnist_model_parallel.py
-
-cd ..
+$run examples/mnist/train_mnist.py
+$run examples/mnist/train_mnist.py --gpu=0
+#$run examples/mnist/train_mnist_model_parallel.py
 
 # ptb
 echo "Running ptb example"
-cd ptb
 
 # change epoch
-sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" train_ptb.py
-sed -i -E "s/^n_units\s*=\s*[0-9]+/n_units = 10/" train_ptb.py
+sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" examples/ptb/train_ptb.py
+sed -i -E "s/^n_units\s*=\s*[0-9]+/n_units = 10/" examples/ptb/train_ptb.py
 # change data size
-sed -i -E "s/^train_data = (.*)/train_data = \1[:100]/" train_ptb.py
-sed -i -E "s/^valid_data = (.*)/valid_data = \1[:100]/" train_ptb.py
-sed -i -E "s/^test_data = (.*)/test_data = \1[:100]/" train_ptb.py
+sed -i -E "s/^train_data = (.*)/train_data = \1[:100]/" examples/ptb/train_ptb.py
+sed -i -E "s/^valid_data = (.*)/valid_data = \1[:100]/" examples/ptb/train_ptb.py
+sed -i -E "s/^test_data = (.*)/test_data = \1[:100]/" examples/ptb/train_ptb.py
 
-python download.py
-python train_ptb.py
-python train_ptb.py --gpu=0
-
-cd ..
+$run examples/ptb/download.py
+$run examples/ptb/train_ptb.py
+$run examples/ptb/train_ptb.py --gpu=0
 
 # sentiment
 echo "Running sentiment example"
-cd sentiment
 
 # change epoch
-sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" train_sentiment.py
+sed -i -E "s/^n_epoch\s*=\s*[0-9]+/n_epoch = 1/" examples/sentiment/train_sentiment.py
 # change data size
-sed -i -E "s/^train_trees = (.*)/train_trees = \1[:10]/" train_sentiment.py
-sed -i -E "s/^test_trees = (.*)/test_trees = \1[:10]/" train_sentiment.py
+sed -i -E "s/^train_trees = (.*)/train_trees = \1[:10]/" examples/sentiment/train_sentiment.py
+sed -i -E "s/^test_trees = (.*)/test_trees = \1[:10]/" examples/sentiment/train_sentiment.py
 
-python download.py
-python train_sentiment.py
-python train_sentiment.py --gpu=0
-
-cd ..
+$run examples/sentiment/download.py
+$run examples/sentiment/train_sentiment.py
+$run examples/sentiment/train_sentiment.py --gpu=0
 
 # imagenet
 echo "Runnig imagenet example"
-cd ../../data/imagenet
 
-python ../../chainer/examples/imagenet/compute_mean.py data.txt
-python ../../chainer/examples/imagenet/train_imagenet.py -a nin data.txt data.txt
-python ../../chainer/examples/imagenet/train_imagenet.py -a alexbn data.txt data.txt
-python ../../chainer/examples/imagenet/train_imagenet.py -a googlenet data.txt data.txt
-python ../../chainer/examples/imagenet/train_imagenet.py -a googlenetbn data.txt data.txt
-
-cd ../../chainer/examples
+imagenet_data=../data/imagenet/data.txt
+$run examples/imagenet/compute_mean.py -r ../data/imagenet $imagenet_data
+$run examples/imagenet/train_imagenet.py -a nin -r ../data/imagenet $imagenet_data $imagenet_data
+$run examples/imagenet/train_imagenet.py -a alexbn -r ../data/imagenet $imagenet_data $imagenet_data
+$run examples/imagenet/train_imagenet.py -a googlenet -r ../data/imagenet $imagenet_data $imagenet_data
+$run examples/imagenet/train_imagenet.py -a googlenetbn -r ../data/imagenet $imagenet_data $imagenet_data
 
 # word2vec
 echo "Running word2vec example"
 
-cd word2vec
 # note that ptb.train.txt is already downloaded
-cp ../ptb/ptb.train.txt .
 
-sed -i -E "s/n_vocab = len\(word2index\)/n_vocab = len(word2index)\ndataset = dataset[:100]/" train_word2vec.py
+sed -i -E "s/n_vocab = len\(word2index\)/n_vocab = len(word2index)\ndataset = dataset[:100]/" examples/word2vec/train_word2vec.py
 
-python train_word2vec.py -e 1 -b 10
-echo "it" | python search.py
-python train_word2vec.py -e 1 -b 10 --gpu=0
-echo "it" | python search.py
-python train_word2vec.py -e 1 -b 10 -m cbow --out-type ns
-echo "it" | python search.py
-python train_word2vec.py -e 1 -b 10 -m cbow --out-type ns --gpu=0
-echo "it" | python search.py
-python train_word2vec.py -e 1 -b 10 --out-type original
-echo "it" | python search.py
-python train_word2vec.py -e 1 -b 10 --out-type original --gpu=0
-echo "it" | python search.py
+$run examples/word2vec/train_word2vec.py -e 1 -b 10
+echo "it" | $run examples/word2vec/search.py
+$run examples/word2vec/train_word2vec.py -e 1 -b 10 --gpu=0
+echo "it" | $run examples/word2vec/search.py
+$run examples/word2vec/train_word2vec.py -e 1 -b 10 -m cbow --out-type ns
+echo "it" | $run examples/word2vec/search.py
+$run examples/word2vec/train_word2vec.py -e 1 -b 10 -m cbow --out-type ns --gpu=0
+echo "it" | $run examples/word2vec/search.py
+$run examples/word2vec/train_word2vec.py -e 1 -b 10 --out-type original
+echo "it" | $run examples/word2vec/search.py
+$run examples/word2vec/train_word2vec.py -e 1 -b 10 --out-type original --gpu=0
+echo "it" | $run examples/word2vec/search.py
 
-cd ..
+# show coverage
+coverage report -m --include="examples/*"
