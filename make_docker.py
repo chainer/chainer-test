e57e8fea4196492955baca426ed0f5654c4c2b94
Yuya Unno <unnonouno@gmail.com>
2015-12-09 18:13:06 +0900

    Install ccche

--- make_docker.py
+++ make_docker.py
@@ -10,7 +10,7 @@ codes['centos7_py2'] = '''FROM centos:7
 
 RUN yum -y update
 RUN yum -y install epel-release
-RUN yum -y install gcc gcc-c++ git kmod hdf5-devel perl
+RUN yum -y install ccache gcc gcc-c++ git kmod hdf5-devel perl
 
 RUN yum -y install python-devel python-pip
 '''
@@ -19,7 +19,7 @@ codes['centos7_py3'] = '''FROM centos:7
 
 RUN yum -y update
 RUN yum -y install epel-release
-RUN yum -y install gcc gcc-c++ git kmod hdf5-devel perl
+RUN yum -y install ccache gcc gcc-c++ git kmod hdf5-devel perl
 
 RUN yum -y install bzip2-devel make openssl-devel readline-devel
 RUN git clone git://github.com/yyuu/pyenv.git /opt/pyenv
@@ -34,7 +34,7 @@ RUN pyenv rehash
 codes['ubuntu14_py2'] = '''FROM ubuntu:14.04
 
 RUN apt-get -y update && apt-get -y upgrade
-RUN apt-get install -y curl g++ gfortran git libhdf5-dev
+RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev
 
 RUN apt-get install -y python-pip python-dev
 '''
@@ -42,7 +42,7 @@ RUN apt-get install -y python-pip python-dev
 codes['ubuntu14_py3'] = '''FROM ubuntu:14.04
 
 RUN apt-get -y update && apt-get -y upgrade
-RUN apt-get install -y curl g++ gfortran git libhdf5-dev
+RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev
 
 RUN apt-get install -y python3-pip python3-dev
 RUN update-alternatives --install /usr/bin/python python /usr/bin/python3 1
@@ -52,7 +52,7 @@ RUN update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 codes['ubuntu14_py35'] = '''FROM ubuntu:14.04
 
 RUN apt-get -y update && apt-get -y upgrade
-RUN apt-get install -y curl g++ gfortran git libhdf5-dev
+RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev
 
 RUN apt-get -y install libbz2-dev libreadline-dev libssl-dev make
 RUN git clone git://github.com/yyuu/pyenv.git /opt/pyenv
