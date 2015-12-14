f32f015516024962ba69e6b773a604bd2ec34eda
Yuya Unno <unnonouno@gmail.com>
2015-12-14 18:06:02 +0900

    Reduce processes

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ python setup.py -q develop install
 
 pip install -q nose mock coverage coveralls
 
-nosetests --processes=8 --process-timeout=10000 --with-coverage --cover-branches --cover-package=chainer,cupy
+nosetests --processes=4 --process-timeout=10000 --with-coverage --cover-branches --cover-package=chainer,cupy
 
 coverage xml -i
 
