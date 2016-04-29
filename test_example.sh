cb8d698a070445d7042099149f7c4deeb6031e7e
Kenta OONO <oono@preferred.jp>
2016-04-29 20:32:04 +0900

    Commented out example other than profiling for test

--- test_example.sh
+++ test_example.sh
@@ -29,52 +29,52 @@ run="coverage run -a --branch"
 echo "Running mnist example"
 
 # change epoch
-$run examples/mnist/train_mnist.py --epoch=1 --unit=10
-$run examples/mnist/train_mnist.py --gpu=0 --epoch=1 --unit=10
-$run examples/mnist/train_mnist.py --net=parallel --epoch=1 --unit=10
+# $run examples/mnist/train_mnist.py --epoch=1 --unit=10
+# $run examples/mnist/train_mnist.py --gpu=0 --epoch=1 --unit=10
+# $run examples/mnist/train_mnist.py --net=parallel --epoch=1 --unit=10
 
 # ptb
 echo "Running ptb example"
 
-$run examples/ptb/download.py
-$run examples/ptb/train_ptb.py --epoch=1 --unit=10 --test
-$run examples/ptb/train_ptb.py --gpu=0 --epoch=1 --unit=10 --test
+# $run examples/ptb/download.py
+# $run examples/ptb/train_ptb.py --epoch=1 --unit=10 --test
+# $run examples/ptb/train_ptb.py --gpu=0 --epoch=1 --unit=10 --test
 
 # sentiment
 echo "Running sentiment example"
 
-$run examples/sentiment/download.py
-$run examples/sentiment/train_sentiment.py --epoch=1 --batchsize=1 --epocheval=1 --test
-$run examples/sentiment/train_sentiment.py --gpu=0 --epoch=1 --batchsize=1 --epocheval=1 --test 
+# $run examples/sentiment/download.py
+# $run examples/sentiment/train_sentiment.py --epoch=1 --batchsize=1 --epocheval=1 --test
+# $run examples/sentiment/train_sentiment.py --gpu=0 --epoch=1 --batchsize=1 --epocheval=1 --test 
 
 # imagenet
 echo "Runnig imagenet example"
 
-imagenet_data=../data/imagenet/data.txt
-$run examples/imagenet/compute_mean.py -r ../data/imagenet $imagenet_data
-$run examples/imagenet/train_imagenet.py --test -a nin -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
-$run examples/imagenet/train_imagenet.py --test -a alex -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
-$run examples/imagenet/train_imagenet.py --test -a alexbn -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
-$run examples/imagenet/train_imagenet.py --test -a googlenet -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
-$run examples/imagenet/train_imagenet.py --test -a googlenetbn -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
+# imagenet_data=../data/imagenet/data.txt
+# $run examples/imagenet/compute_mean.py -r ../data/imagenet $imagenet_data
+# $run examples/imagenet/train_imagenet.py --test -a nin -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
+# $run examples/imagenet/train_imagenet.py --test -a alex -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
+# $run examples/imagenet/train_imagenet.py --test -a alexbn -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
+# $run examples/imagenet/train_imagenet.py --test -a googlenet -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
+# $run examples/imagenet/train_imagenet.py --test -a googlenetbn -r ../data/imagenet -B 1 -b 1 -E 1 $imagenet_data $imagenet_data
 
 # word2vec
 echo "Running word2vec example"
 
 # note that ptb.train.txt is already downloaded
 
-$run examples/word2vec/train_word2vec.py -e 1 -b 10 --test
-echo "it" | $run examples/word2vec/search.py
-$run examples/word2vec/train_word2vec.py -e 1 -b 10 --gpu=0 --test
-echo "it" | $run examples/word2vec/search.py
-$run examples/word2vec/train_word2vec.py -e 1 -b 10 -m cbow --out-type ns --test
-echo "it" | $run examples/word2vec/search.py
-$run examples/word2vec/train_word2vec.py -e 1 -b 10 -m cbow --out-type ns --gpu=0 --test
-echo "it" | $run examples/word2vec/search.py
-$run examples/word2vec/train_word2vec.py -e 1 -b 10 --out-type original --test
-echo "it" | $run examples/word2vec/search.py
-$run examples/word2vec/train_word2vec.py -e 1 -b 10 --out-type original --gpu=0 --test
-echo "it" | $run examples/word2vec/search.py
+# $run examples/word2vec/train_word2vec.py -e 1 -b 10 --test
+# echo "it" | $run examples/word2vec/search.py
+# $run examples/word2vec/train_word2vec.py -e 1 -b 10 --gpu=0 --test
+# echo "it" | $run examples/word2vec/search.py
+# $run examples/word2vec/train_word2vec.py -e 1 -b 10 -m cbow --out-type ns --test
+# echo "it" | $run examples/word2vec/search.py
+# $run examples/word2vec/train_word2vec.py -e 1 -b 10 -m cbow --out-type ns --gpu=0 --test
+# echo "it" | $run examples/word2vec/search.py
+# $run examples/word2vec/train_word2vec.py -e 1 -b 10 --out-type original --test
+# echo "it" | $run examples/word2vec/search.py
+# $run examples/word2vec/train_word2vec.py -e 1 -b 10 --out-type original --gpu=0 --test
+# echo "it" | $run examples/word2vec/search.py
 
 # profiling
 $run examples/profile/profile.py -i 1 -g -1
