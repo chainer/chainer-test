d3dcb76ec2ae3351866753484ce70e5b99ff8498
Yuya Unno <unnonouno@gmail.com>
2016-06-01 01:43:36 +0900

    Fix dependent libraryies

--- docker.py
+++ docker.py
@@ -108,7 +108,7 @@ RUN pyenv rehash
 codes['ubuntu16_py2'] = '''FROM ubuntu:16.04
 
 RUN apt-get -y update && apt-get -y upgrade
-RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev
+RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev libhdf5-serial-dev pkg-config
 
 ENV PATH /usr/lib/ccache:$PATH
 
@@ -118,7 +118,7 @@ RUN apt-get install -y python-pip python-dev
 codes['ubuntu16_py3'] = '''FROM ubuntu:16.04
 
 RUN apt-get -y update && apt-get -y upgrade
-RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev
+RUN apt-get install -y ccache curl g++ gfortran git libhdf5-dev libhdf5-serial-dev pkg-config
 
 ENV PATH /usr/lib/ccache:$PATH
 
