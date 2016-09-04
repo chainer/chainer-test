c853fd6c906dbb171e3e5f8e7d96c4b7aa493358
Yuya Unno <unnonouno@gmail.com>
2016-09-04 16:16:34 +0900

    Use png file for test as it is required no other packages such as libjpeg

--- test_example.sh
+++ test_example.sh
@@ -9,18 +9,6 @@ cd dist
 pip install *.tar.gz --user
 cd ..
 
-if [ -e cuda_deps/setup.py ]; then
-  python cuda_deps/setup.py -q install
-fi
-
-if apt-get --version; then
-  apt-get install -y libjpeg-dev zlib1g-dev
-elif yum --version; then
-  yum -y install libjpeg-devel zlib-devel
-else
-  echo "both apt-get and yum command are not found"
-  exit 1
-fi
 pip install coverage pillow --user
 
 run="coverage run -a --branch"
