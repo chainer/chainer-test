e67d98dd4e1ed6ca9b5c45957aa7fec1d2b7a182
Gentaro Watanabe <g.wtnb@preferred.jp>
2017-02-07 12:48:56 +0900

    Add comment

--- run_combination_test.py
+++ run_combination_test.py
@@ -71,6 +71,7 @@ if __name__ == '__main__':
     if params['h5py'] == '2.6':
         conf['requires'].append('h5py<2.7')
     elif params['h5py'] == '2.5':
+        # h5py uses numpy in its setup script
         conf['requires'].append('numpy<1.10')
         conf['requires'].append('h5py<2.6')
 
