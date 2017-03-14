dadb703efa3273d20c589e3fb47858fda3313706
Yuya Unno <unnonouno@gmail.com>
2017-03-14 16:40:33 +0900

    Make scripts for cupy

---
+++ test_cupy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+
+cd cupy
+flake8
+python setup.py build -j 4 develop install --user || python setup.py develop install --user
+
+if [ $CUDNN = none ]; then
+  nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=cupy -a '!cudnn,!slow'
+else
+  nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=cupy -a '!slow'
+fi
+
+coverage xml -i
+
+if [ $COVERALLS_REPO_TOKEN ]; then
+  coveralls
+fi
