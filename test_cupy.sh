25a7b9b0009d6ce50244c2c4e7da9dfc64d9998d
Ryosuke Okuta <okuta@preferred.jp>
2017-03-27 00:45:53 +0900

    Fix ignoring FutureWarnings

--- test_cupy.sh
+++ test_cupy.sh
@@ -4,6 +4,8 @@ cd cupy
 flake8
 python setup.py build -j 4 develop install --user || python setup.py develop install --user
 
+export PYTHONWARNINGS="ignore::FutureWarning"
+
 if [ $CUDNN = none ]; then
   nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=cupy -a '!cudnn,!slow'
 else
