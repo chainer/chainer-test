e54239810b2cb108e5a63cd16f1f3d9189632993
Yuya Unno <unnonouno@gmail.com>
2017-05-21 17:04:00 +0900

    Add test script for cupy installation

---
+++ test_cupy_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -ex
+
+timeout 20m pip install -vvvv cupy/dist/*.tar.gz --user
+
+# check if cupy is installed
+if [ -z "$CUDNN_VER" ]; then
+  python -c 'import cupy'
+else
+  python -c 'import cupy.cuda.cudnn; print(cupy.cuda.cudnn.getVersion())'
+fi
