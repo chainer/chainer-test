69a3fed29b429eda9287fb7906dd3f9a7ae24923
Yuya Unno <unnonouno@gmail.com>
2017-08-21 11:03:22 +0900

    Make a helper function

--- run_cupy_combination_test.py
+++ run_cupy_combination_test.py
@@ -7,26 +7,11 @@ import random
 import argconfig
 import docker
 import shuffle
-import version
-
-
-cuda_cudnn_choices = [
-    (cuda, cudnn) for cuda, cudnn in docker.cuda_cudnn_choices
-    if cuda != 'none']
-
-cupy_major = version.get_cupy_version()[0]
-
-if cupy_major <= 1:
-    cuda_cudnn = [
-        (cuda, cudnn) for cuda, cudnn in cuda_cudnn_choices
-        if cuda != 'cuda90' and 'cudnn7' not in cudnn]
-else:
-    cuda_cudnn = docker.cuda_cudnn_choices
 
 
 params = {
     'base': docker.base_choices,
-    'cuda_cudnn': cuda_cudnn_choices,
+    'cuda_cudnn': docker.get_cuda_cudnn_choices('cupy'),
     'nccl': docker.nccl_choices,
     'numpy': ['1.9', '1.10', '1.11', '1.12'],
     'scipy': [None, '0.18'],
