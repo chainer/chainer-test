5e0b8e459aced139d8d14a9a053495095be659c1
Yuya Unno <unnonouno@gmail.com>
2017-09-08 11:13:07 +0900

    Disable coverage

--- test.sh
+++ test.sh
@@ -10,9 +10,9 @@ python setup.py install --user
 export PYTHONWARNINGS="ignore::FutureWarning"
 
 if [ $CUDNN = none ]; then
-  nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=chainer -a '!cudnn,!slow' tests
+  nosetests --processes=4 --process-timeout=10000 --stop -a '!cudnn,!slow' tests
 else
-  nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=chainer -a '!slow' tests
+  nosetests --processes=4 --process-timeout=10000 --stop -a '!slow' tests
 fi
 
 python ../push_coveralls.py
