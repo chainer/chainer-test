0972087bb2f4d27a367d3a6544fde2d9e960906f
Yuya Unno <unnonouno@gmail.com>
2017-09-10 12:09:10 +0900

    Use one process for each test

--- test_cupy.sh
+++ test_cupy.sh
@@ -6,9 +6,9 @@ python setup.py build -j 4 develop install --user || python setup.py develop ins
 export PYTHONWARNINGS="ignore::FutureWarning"
 
 if [ $CUDNN = none ]; then
-  nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=cupy -a '!cudnn,!slow' tests
+  nosetests --stop --with-coverage --cover-branches --cover-package=cupy -a '!cudnn,!slow' tests
 else
-  nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=cupy -a '!slow' tests
+  nosetests --stop --with-coverage --cover-branches --cover-package=cupy -a '!slow' tests
 fi
 
 python ../push_coveralls.py
