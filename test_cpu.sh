3a3b0b01ff6e2fb312b2c1dc562bb7c62c87461a
niboshi <niboshi000@gmail.com>
2017-09-19 01:12:00 +0900

    Set CUPY_DUMP_CUDA_SOURCE_ON_ERROR

--- test_cpu.sh
+++ test_cpu.sh
@@ -5,4 +5,6 @@ cd chainer
 python setup.py develop install --user
 
 export PYTHONWARNINGS="ignore::FutureWarning"
+export CUPY_DUMP_CUDA_SOURCE_ON_ERROR=1
+
 nosetests --processes=4 --process-timeout=10000 -a '!gpu,!slow' --stop --with-coverage --cover-branches --cover-package=chainer tests/chainer_tests
--- test_cupy.sh
+++ test_cupy.sh
@@ -4,6 +4,7 @@ cd cupy
 python setup.py build -j 4 develop install --user || python setup.py develop install --user
 
 export PYTHONWARNINGS="ignore::FutureWarning"
+export CUPY_DUMP_CUDA_SOURCE_ON_ERROR=1
 
 if [ $CUDNN = none ]; then
   nosetests --stop --with-coverage --cover-branches --cover-package=cupy -a '!cudnn,!slow' tests
