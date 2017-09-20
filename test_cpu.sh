2adf3e1896f1a023068372bd657cd5bc2b049750
niboshi <niboshi000@gmail.com>
2017-09-20 09:26:02 +0900

    Do not stop on error

--- test_cpu.sh
+++ test_cpu.sh
@@ -5,4 +5,4 @@ cd chainer
 python setup.py develop install --user
 
 export PYTHONWARNINGS="ignore::FutureWarning"
-nosetests --processes=4 --process-timeout=10000 -a '!gpu,!slow' --stop --with-coverage --cover-branches --cover-package=chainer tests/chainer_tests
+nosetests --processes=4 --process-timeout=10000 -a '!gpu,!slow' --with-coverage --cover-branches --cover-package=chainer tests/chainer_tests
