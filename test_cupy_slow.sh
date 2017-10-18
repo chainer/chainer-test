1436c90c67302fd5b79573c3bf55cc73c4c3e88b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-10-18 11:15:46 +0900

    use pytest

--- test_cupy_slow.sh
+++ test_cupy_slow.sh
@@ -7,7 +7,7 @@ export PYTHONWARNINGS="ignore::FutureWarning"
 export CUPY_DUMP_CUDA_SOURCE_ON_ERROR=1
 
 if [ $CUDNN = none ]; then
-  nosetests --with-coverage --cover-branches --cover-package=cupy -a '!cudnn,slow' tests
+  python -m pytest --cov-report=xml --cov -m 'slow and not cudnn' tests
 else
-  nosetests --with-coverage --cover-branches --cover-package=cupy -a 'slow' tests
+  python -m pytest --cov-report=xml --cov -m 'slow' tests
 fi
