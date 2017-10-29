3a3b0a0dc7aea658b769db4c588cdd26554aca9c
Ryosuke Okuta <okuta@preferred.jp>
2017-10-29 23:50:44 +0900

    Fix doctest

--- test_cupy_doc.sh
+++ test_cupy_doc.sh
@@ -6,4 +6,6 @@ python setup.py -q build -j 4 develop install --user || python setup.py -q devel
 cd docs
 make html
 make clean
+# The doctest has some bug. We need two pass doctest
+make doctest
 make doctest
