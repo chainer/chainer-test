e428f054187c5f6439bcaff99ea437307aad7577
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-04-17 19:15:29 +0900

    use latest iDeep4py 1.0.x

--- run_multi_test.py
+++ run_multi_test.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser.add_argument('--cuda', choices=docker.cuda_choices, required=True)
     parser.add_argument('--cudnn', choices=docker.cudnn_choices, required=True)
     parser.add_argument('--nccl', choices=docker.nccl_choices, required=True)
-    parser.add_argument('--ideep', choices=['none', '1.0.1'], required=True)
+    parser.add_argument('--ideep', choices=['none', '1.0'], required=True)
     parser.add_argument('--numpy', choices=['1.9', '1.10', '1.11', '1.12'],
                         required=True)
     parser.add_argument('--protobuf', choices=['2', '3', 'cpp-3'])
@@ -90,8 +90,8 @@ if __name__ == '__main__':
     elif args.pillow == '4.1':
         conf['requires'].append('pillow<4.2')
 
-    if args.ideep != 'none':
-        conf['requires'].append('ideep4py=={}'.format(args.ideep))
+    if args.ideep == '1.0':
+        conf['requires'].append('ideep4py<1.1')
 
     use_ideep = any(['ideep4py' in req for req in conf['requires']])
 
--- run_test.py
+++ run_test.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
             'requires': [
                 'setuptools', 'cython==0.28.1', 'numpy<1.14',
                 'scipy<0.19', 'h5py', 'theano', 'protobuf<3',
-                'ideep4py==1.0.3',
+                'ideep4py<1.1',
             ],
         }
         script = './test.sh'
@@ -88,7 +88,7 @@ if __name__ == '__main__':
                 'setuptools', 'cython==0.26.1', 'numpy<1.14',
                 'scipy<0.19', 'h5py', 'theano', 'protobuf<3',
                 'pillow',
-                'ideep4py==1.0.3',
+                'ideep4py<1.1',
             ],
         }
         script = './test_slow.sh'
@@ -128,7 +128,7 @@ if __name__ == '__main__':
                 'pip==9.0.1', 'setuptools', 'cython==0.26.1', 'matplotlib',
                 'numpy>=1.14', 'scipy<0.19', 'theano', 'sphinx==1.5.3',
                 'sphinx_rtd_theme',
-                'ideep4py==1.0.3',
+                'ideep4py<1.1',
             ]
         }
         script = './test_doc.sh'
