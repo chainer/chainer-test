d106e11cb247c704817f4f1e8678593b8c845f40
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-07-03 16:15:45 +0900

    use gcc-7 on Ubuntu 18

--- environment.sh
+++ environment.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# CuPy v8 requires g++-6 or later to build CUB.
 if [ "${CUPY_V8}" != 0 ]; then
     if [ -f /opt/rh/devtoolset-6/enable ]; then
-        source /opt/rh/devtoolset-6/enable
+        # For CentOS 7, load environment variables for devtoolset-6.
+        . /opt/rh/devtoolset-6/enable
     fi
-    export NVCC="${NVCC} --compiler-bindir gcc-6"
+
+    if which g++-6; then
+        # For Ubuntu 16.04 and CentOS 7, use g++-6.
+        # For Ubuntu 18.04, uses the default g++-7.
+        export NVCC="${NVCC} --compiler-bindir gcc-6"
+    fi
 fi
