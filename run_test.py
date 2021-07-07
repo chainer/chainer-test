e47c9315862dc0c56655820b3a3ac0dec04c99ae
Emilio Castillo <ecastill@preferred.jp>
2021-07-07 06:28:22 +0000

    Bump numpy to 1.12

--- run_test.py
+++ run_test.py
@@ -255,7 +255,7 @@ def main():
             'cusparselt': 'none',
             'requires': [
                 'setuptools<42', 'pip', 'cython==0.29.22',
-                'numpy==1.19.*', 'scipy==1.6.*',
+                'numpy==1.21.*', 'scipy==1.6.*',
             ] + requires,
         }
         script = './test_cupy.sh'
@@ -271,7 +271,7 @@ def main():
             'requires': [
                 # TODO(kmaehashi): Remove setuptools version restrictions
                 # https://github.com/chainer/chainer-test/issues/565
-                'setuptools<42', 'pip', 'cython==0.29.22', 'numpy==1.19.*',
+                'setuptools<42', 'pip', 'cython==0.29.22', 'numpy==1.21.*',
             ],
         }
         script = './test_cupy.sh'
@@ -288,7 +288,7 @@ def main():
             'requires': [
                 # TODO(kmaehashi): Remove setuptools version restrictions
                 # https://github.com/chainer/chainer-test/issues/565
-                'setuptools<42', 'pip', 'cython==0.29.22', 'numpy==1.19.*',
+                'setuptools<42', 'pip', 'cython==0.29.22', 'numpy==1.21.*',
             ],
         }
         script = './test_cupy.sh'
@@ -371,7 +371,7 @@ def main():
                     # TODO(kmaehashi): Remove setuptools version restrictions
                     # https://github.com/pypa/setuptools/issues/2352
                     'setuptools<50', 'cython==0.29.22',
-                    'numpy==1.19.*', 'scipy==1.6.*',
+                    'numpy==1.21.*', 'scipy==1.6.*',
                 ],
             }
         script = './test_cupy_example.sh'
@@ -395,7 +395,7 @@ def main():
                 # TODO(kmaehashi): Remove setuptools version restrictions
                 # https://github.com/pypa/setuptools/issues/2352
                 'pip==9.0.1', 'setuptools<50', 'cython==0.29.22',
-                'numpy==1.20.*', 'scipy==1.6.*', 'wheel==0.36.2'
+                'numpy==1.21.*', 'scipy==1.6.*', 'wheel==0.36.2'
             ] + requires + SPHINX_REQUIREMENTS_PIP
         }
         script = './test_cupy_doc.sh'
